24665e0ea118b420be6776cda9a86dbc512334fb
sparis4 <sparis4@msudenver.edu>
2020-03-10 19:51:45 -0600

    Working import file

--- importing.py
+++ importing.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 #Importing libraries needed to read in an image
 import cv2
-import numpy as np
-import matplotlib.pyplot as plt
 
+
+#def get_file():
+value = input("File Path Paramaters /path_to_image/image.extension: ")      #Request File path and provide paramters 
+image = cv2.imread(value)                                                   #Read image
+cv2.imshow("image",image)                                                   #display image
+cv2.waitKey(0)                                                              #Wait for user to input any value to quit.
