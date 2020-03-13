cdf2038cfda02c7160da1121e72e4b713df5faab
Taylor Dean <36752623+TMichelle6154@users.noreply.github.com>
2020-03-13 11:49:17 -0600

    some changes. Haven't added errorhandling and still looking to make changes.

--- gui.py
+++ gui.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python3
+
+from tkinter import *
+
+# pip install pillow
+from PIL import Image, ImageTK 
+
+class Window(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+        self.pack(fill=BOTH, expand=1)
+        
+        load = Image.open(image)  #placeholder generic image till better defined
+        render = ImageTk.PhotoImage(load)
+        img = Label(self, image=render)
+        img.image = render
+        img.place(x=0, y=0)
+
+        
+root = Tk()
+app = Window(root)
+root.wm_title("Tkinter window")
+root.geometry("200x120")
+root.mainloop()
