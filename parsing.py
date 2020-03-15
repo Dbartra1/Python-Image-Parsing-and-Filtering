09dbadcb10148b58a8c0280ad67ac5b11ede1f06
Taylor Dean <36752623+TMichelle6154@users.noreply.github.com>
2020-03-14 21:38:08 -0600

    Update parsing.py

--- parsing.py
+++ parsing.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python3
 import numpy as np
 import cv2
+import color_space
 
  #def parse_image():
 image = importing.get_file()  #get the file from importing.
 _, frame = image.read() #read the image
+
+#resize to make make process faster so less pixels for program to solve
+modified_image = cv2.resize(image, (600, 400), interpolation = cv2.INTER_AREA)
+modified_image = modified_image.reshape(modified_image.shape[0]*modified_image.shape[1], 3)
+
 hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) #convert colours to Hue Saturation Value
 
- 
 #As I don't yet know how to get the pointer to select a specific hue, the user will manually enter the color until later on in the project. Prototype 1
 #Gets the upper bound of the desired colour(s)
-user_choice_upperbound = np.array([int(input("Put the higher red value here (1-255)")), int(input("Put the higher green value here (1-255)")), int(input("Put the higher blue value here (1-255)"))])
+#user_choice_upperbound_r = np.array([int(input("Put the higher red value here (1-255)")), int(input("Put the higher green value here (1-255)")), int(input("Put the higher blue value here (1-255)"))])
 #Gets the lower bound of the desired colour(s)
-user_choice_lowerbound = np.array([int(input("Put the lower red value here (1-255)")), int(input("Put the lower green value here (1-255)")), int(input("Put the lower blue value here (1-255)"))])
+#user_choice_lowerbound_r = np.array([int(input("Put the lower red value here (1-255)")), int(input("Put the lower green value here (1-255)")), int(input("Put the lower blue value here (1-255)"))])
 
- 
+
 
 #masks anything not the desired colour(s)
-mask = cv2.inRange(hsv, user_choice_lowerbound, user_choice_upperbound)
-res = cv2.bitwise_and(frame, frame, mask = mask)
+mask_r = cv2.inRange(hsv, red_lower, red_upper)
+res_r = cv2.bitwise_and(frame, frame, mask_r = mask_r)
+mask_b = cv2.inRange(hsv, blue_lower, blue_upper)
+res_b = cv2.bitwise_and(frame, frame, mask_b = mask_b)
+mask_y = cv2.inRange(hsv, yellow_lower, yellow_upper)
+res_y = cv2.bitwise_and(frame, frame, mask_y = mask_y)
