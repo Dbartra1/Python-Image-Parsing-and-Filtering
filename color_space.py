c06ec3198405a25ea0e3e2c42f9b6a7f938d86c2
Taylor Dean <36752623+TMichelle6154@users.noreply.github.com>
2020-03-14 21:37:05 -0600

    Update color_space.py

--- color_space.py
+++ color_space.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+
+import cv2
+import parsing
+
+
+#make masks of all colors
+
+#defining the range red
+red_lower=np.array([136,87,111])
+red_upper=np.array([180,255,255])
+#defining the range blue
+blue_lower=np.array([99,115,150])
+blue_upper=np.array([110,255,255])
+#defining the yellow range
+yellow_lower=np.array([22,60,200])
+yellow_upper=np.array([60,255,255])
+
