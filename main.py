c1aed1c9ce30b9487e83286afed917fdeb4f4e0c
sparis4 <61263592+sparis4@users.noreply.github.com>
2020-03-17 19:36:19 -0600

    Update main.py
    created main to be functional

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
-import importing
+import importing as imp
+
+def main():
+    image_path = imp.get_file()
+    image = imp.conv_image(image_path)
+    imp.get_parseImage(image)
+   
+
+if __name__ == "__main__":
+    main()
