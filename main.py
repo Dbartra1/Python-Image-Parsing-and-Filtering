1fbc8df34b72067250df2404b9bf7fa323a8b58a
sparis4 <61263592+sparis4@users.noreply.github.com>
2020-03-20 20:16:53 -0600

    Update main.py
    Added Credits

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
     image_path = imp.get_file()
     image = imp.conv_image(image_path)
     imp.get_parseImage(image)
+    print("\nCreated by:\tDillon Bartram, Edward Aylott, Sorrel Paris, Taylor Dean")            #Credits
    
 
 if __name__ == "__main__":
