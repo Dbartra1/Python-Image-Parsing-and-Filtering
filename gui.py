f1d166c1e12d996b40c1f26e974de307378ca003
sparis4 <61263592+sparis4@users.noreply.github.com>
2020-04-14 21:51:01 -0600

    Update gui.py
    in progress.

--- gui.py
+++ gui.py
@@ -1,28 +1,23 @@
 #!/usr/bin/env python3
-
 # importing libraries 
-import cv2
-import numby
-import tkinter
-import color_space
+import parsing as parse
+import importing as imp
 
-windowName ="Open CV Color Palette"
+def displayMenu():
+    print("COMMAND MENU")
+    listOfOptions = """
+    list - Display prior converstions
+    edit - Modify Image
+    """
+def listPaths(history):
+    if len(history) == 0:
+        print("The file contains no paths.\n")
+        return
+    else:
+        i = 0
+        for path in history:
+            print(str(i+1) + ". " + path[0])
+            i += 1
+        print()
 
-# window name 
-cv2.namedWindow(windowName)  
-       
-# Used to open the window 
-cv2.imshow(windowName, image) 
-
-button_red = tkk.Button(frame, text="Red", command=click_red)
-button_blue = tkk.Button(frame, text="Blue", command=click_blue)
-button_green = tkk.Button(frame, text="Green", command=click_yellow)
-
-def click_red():
-    cv2.imshow(res_r)
-
-def click_blue():
-    cv2.imshow(res_b)
-
-def click_yellow():
-    cv2.imshow(res_y)
+def edit(image_path):
