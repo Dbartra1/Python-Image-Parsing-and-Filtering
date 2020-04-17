523116cc7873590dbbcb43f59ef58358c2eb2d19
sparis4 <61263592+sparis4@users.noreply.github.com>
2020-04-17 12:45:44 -0600

    Update color_space.py
    Changed variable names. Added Functionality - to compare images through the use of Numpy's concatenate. Created user input validation checks to ensure acceptable values are entered.

--- color_space.py
+++ color_space.py
@@ -5,10 +5,10 @@ import numpy as np
 
 def modifyImageSize(image_path):
 
-    image = cv2.imread(image_path)                                                      #Reading image from verified file path
+    image = cv2.imread(image_path)                                                          #Reading image from verified file path
 
     while True:
-            w, h, c = image.shape                                                              #Extracting two variables from a tuple
+            w, h, c = image.shape                                                           #Extracting two variables from a tuple
             while True:
                 scale_percent = int(input("Enter a value between 1 and 200:  "))            #Percent of Original size / Dimensions
                 try:
@@ -30,45 +30,79 @@ def modifyImageSize(image_path):
     return image
 
 def modifyImageColor(image):
+    #Initializing variables 
     w, h, c = image.shape
     inputs = []
 
     while True:
         try:
+            #Retrieve user input 
             blue = int(input("Filter perctange of Blue (0 to 255): "))
-            colorBlue = str(blue)
-            blueWords = "Blue = "
-            combinedWordBlue = blueWords + colorBlue
-            inputs.append(combinedWordBlue)
             green = int(input("Filter perctange of Green (0 to 255): "))
-            colorGreen = str(green)
-            greenWords = "Green = "
-            combinedWordGreen = greenWords + colorGreen
-            inputs.append(combinedWordGreen)
             red = int(input("Filter perctange of Red (0 to 255): "))
-            colorRed = str(red)
-            redWords = "Red = "
-            combinedWordRed = redWords + colorRed
-            inputs.append(combinedWordRed)  
-            if blue < 0 and green < 0 and red < 0 and blue > 255 and green > 255 and red > 255:
+            opacityPercent = float(input("Enter percentage of opacity (.001 to 1.0): "))
+
+            #Ensure user input is valid before it is put into the list for records
+            if blue >= 0 and green >= 0 and red >= 0 and opacityPercent >= .001 and blue <= 255 and green <= 255 and red <= 255 and opacityPercent <= 1.0:
+
+                #Change Integer values into String values & append to master list
+                sBlue = str(blue)
+                sPrefixBlue = "Blue = "
+                bCombined = sPrefixBlue + sBlue
+                inputs.append(bCombined)
+
+                sGreen = str(green)
+                gPrefixGreen = "Green = "
+                gCombined = gPrefixGreen + sGreen
+                inputs.append(gCombined)
+
+                sRed = str(red)
+                sPrefixRed = "Red = "
+                rCombined = sPrefixRed + sRed
+                inputs.append(rCombined)
+
+                sOpacity = str(opacityPercent)
+                sPrefixOpacity = "Opacity = "
+                oCombined = sPrefixOpacity + sOpacity
+                inputs.append(oCombined)  
+                break
+
+            else:
                 print("Invalid Input, Try again\n")
                 continue
-            else:
-                break
-        except ValueError:
-            print("Invalid Input, Try again\n")
 
-    opacityPercent = float(input("Enter percentage of opacity (.001 to 1.0): "))
-    opacity = str(opacityPercent)
-    opacityWords = "Opacity = "
-    combinedOpacity = opacityWords + opacity
-    inputs.append(combinedOpacity)  
+        except ValueError:
+            print("Invalid Input, Try again\n") 
+
+    #Creating filtered image through user assigned variables.
+    filteredImage =  np.full((w, h, c), (blue, green, red), np.uint8)                            
+    finalImage = cv2.addWeighted(image, 1, filteredImage, opacityPercent, 0)
     
-    redImage =  np.full((w, h, c), (blue, green, red), np.uint8)                            #the scale size must be known. the color also has to change
-    fusedImage = cv2.addWeighted(image, 1, redImage, opacityPercent, 0)
+    #Naming window variables.
+    origImageWinName = "Original Image (Resized)"
+    filteredImageWinName = "Filtered Image"
+    comparisonImageWinName = "Comparison"
     
-    cv2.imshow("default",  fusedImage)                                                    #Display image to screen with window named Gray Scale
-    cv2.waitKey(0)
-    cv2.destroyWindow("default")
+    #Numpy's concatenate combines two images - will be used for side by side comparison.
+    sideBySide = np.concatenate((finalImage, image), axis = 1)
+
+    #Use of a loop to determine how to present the filtered image.
+    while True:
+        choice = input("\nHow would you like to view your filtered image?"  + 
+                       " (compared to original (1) or by itself (2): ")
+        if choice == "1":
+            cv2.imshow(comparisonImageWinName, sideBySide)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+            break
+        elif choice == "2":
+            cv2.imshow(filteredImageWinName,  finalImage)                                                       
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+            break
+        else:
+            print("Invalid Input, Try again\n")
+            continue
+        
 
     return inputs
