de0ac78a6ac7bbdda41d729b33f3fca59f55443b
sparis4 <61263592+sparis4@users.noreply.github.com>
2020-04-17 23:33:47 -0600

    Update gui.py
    Realistically an not needed module, but to keep things separate and to make it realistic for future development made basic terminal window menu.

--- gui.py
+++ gui.py
@@ -1,23 +1,12 @@
 #!/usr/bin/env python3
-# importing libraries 
-import parsing as parse
-import importing as imp
 
 def displayMenu():
-    print("COMMAND MENU")
-    listOfOptions = """
-    list - Display prior converstions
-    edit - Modify Image
-    """
-def listPaths(history):
-    if len(history) == 0:
-        print("The file contains no paths.\n")
-        return
-    else:
-        i = 0
-        for path in history:
-            print(str(i+1) + ". " + path[0])
-            i += 1
-        print()
 
-def edit(image_path):
+    listOfOptions = """
+    \t\t\t\tMenu
+
+    History -\tDisplay past paramters and files.
+    New -\tSelect a new image for modification.
+    Quit -\tQuits Program.
+    """
+    print(listOfOptions)
