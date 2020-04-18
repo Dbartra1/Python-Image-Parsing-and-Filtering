8cd6dcfbc71fa4d01aa4617e2acc2b82a85e2262
sparis4 <61263592+sparis4@users.noreply.github.com>
2020-04-17 23:30:37 -0600

    Update main.py
    Added in necessary code for the terminal based menu to work properly.

--- main.py
+++ main.py
@@ -2,19 +2,57 @@
 import importing as imp
 import parsing as parse
 import color_space as cs
+import gui
 
 def main():
-    while True:
-        image_path = imp.get_file()
-        history = parse.saveFileLocation(image_path)
-        image = cs.modifyImageSize(image_path)
-        inputs = cs.modifyImageColor(image)
-        parse.saveUsrInput(inputs)
+    gui.displayMenu()
 
-        result = input("Continue? (y/n): ")                                                    #Allowing for the program to run as many times as needed.
-        print()
-        if result.lower () != "y":
-            break
+    while True:
+        parse.readInHistory()
+        while True:
+            selection = input("Please select a Menu Item to begin: ")
+            try:
+                if selection.casefold() == "new":
+
+                    image_path = imp.get_file()
+                    parse.saveFileLocation(image_path)
+                    image = cs.modifyImageSize(image_path)
+                    inputs = cs.modifyImageColor(image)
+                    parse.saveUsrInput(inputs)
+
+                    result = input("Go back to the main menu? (y/n): ")                                                  #Allowing for the program to run as many times as needed.
+                    print()
+                    if result.casefold() == "y":
+                        parse.readInHistory()                                                                            #Read in csv for updated list.
+                        gui.displayMenu()
+                        continue
+                    else:
+                        break
+        
+                elif selection.casefold() == "history":
+
+                    history1 = parse.removeCharacters()
+                    print(history1)
+
+                    result = input("Go back to the main menu? (y/n): ")                                                  #Allowing for the program to run as many times as needed.
+                    print()
+                    if result.casefold() == "y":
+                        gui.displayMenu()
+                        continue
+                    else:
+                        break
+        
+                elif selection.casefold() == "quit":
+                    break
+
+                else:
+                    print("Invalid Input, Try again\n")
+                    continue
+
+            except ValueError:
+                print("Invalid Input, Try again\n")
+
+        break
     print("\nCreated by: Dillon Bartram, Edward Aylott, Sorrel Paris, Taylor Dean")            #Credits
    
 
